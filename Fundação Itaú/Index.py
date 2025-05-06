afea8c6a20499f81954ec11da0c0a11d2da0c0ff
Matheusctt1Vent <matheuscosta@vent.digital>
2025-05-06 14:04:15 -0300

    Update Index.py

--- Fundação Itaú/Index.py
+++ Fundação Itaú/Index.py
@@ -23,7 +23,7 @@ pages = {
 pg = st.navigation(pages)
 pg.run()
 
-st.logo(st.logo('/imagens/Logo.png', size='Large', icon_image='./imagens/Logo.png')
+st.logo(st.logo('/imagens/Logo.png', size='Large', icon_image='/imagens/Logo.png')
 )
 
 with st.sidebar.popover('Matheus', icon=":material/person:", use_container_width=False):
